d9617b417ffd342f0a3f2acc4b7d7fbd2df38a37
jhenriquedsilva <josehs555@gmail.com>
2021-07-30 21:37:23 -0300

    Finishing exercise 2

--- src/main/kotlin/exercise_two/segments.kt
+++ src/main/kotlin/exercise_two/segments.kt
@@ -2,27 +2,29 @@ package exercise_two
 
 // Question 2
 fun main() {
-    println(segment("at","I'm great at football"))
-    println(countOcurrences("a", "I'm great at football"))
+    println(segment("at","I'm great at football")) // true
+    println(segment("ta","I'm great at football")) // flase
+    println(countOcurrences("a", "I'm great at football")) // 3
+    println(countOcurrences("h", "I'm great at football")) // 0
 }
 
-// Verifies if segment(s) is within text(t)
+// Verifies if segment is within text
 fun segment(segment: String, text: String): Boolean {
 
-    // Slice will have the same size as segment to check if slice is equal to segment in one of the iterations inside text
-    var slice = ""
-    val lastIndexOfText = text.lastIndex
+    var slice = "" // This mutable variable will be updated with substrings from text to check if it is equal to segment when iterationg over text
+    val lastIndexOfText = text.lastIndex // This immutable variable prevents an Out of Bound Exception
+    // By these two mutable variables, the algorithm selects a different substring in each iteration
     var start = 0
     var end = segment.lastIndex
 
     // Iteration has to stop at the end of text
     while (end <= lastIndexOfText) {
-        // Slice is updated in each iteration with the value of new part of text
-        slice = text.slice(start..end)
+        // Slice is updated with a new substring in each iteration
+        slice = text.slice(start..end) // This is a range
         if (slice == segment) {
             return true
         } else {
-            // Start and end are updated
+            // "Start" and "end" are updated to get a new substring next iteration
             start++
             end++
         }
@@ -32,23 +34,26 @@ fun segment(segment: String, text: String): Boolean {
 
 // Check how many times segment(s) is within text(t)
 fun countOcurrences(segment: String, text: String): Int {
-    // Slice will have the same size as segment to check if slice is equal to segment in one of the iterations inside text
-    var slice = ""
-    val lastIndexOfText = text.lastIndex
+
+    var slice = "" // This mutable variable will be updated with substrings from text to check if it is equal to segment when iterationg over text
+    val lastIndexOfText = text.lastIndex // This immutable variable prevents an Out of Bound Exception
+    // By these two mutable variables, the algorithm selects a different substring in each iteration
     var start = 0
     var end = segment.lastIndex
-    var counter = 0 // Initially counter equals 0
+    var counter = 0 // Counts how many times segment is within text
 
     // Iteration has to stop at the end of text
     while (end <= lastIndexOfText) {
-        // Slice is updated in each iteration with the value of new part of text
-        slice = text.slice(start..end)
+        // Slice is updated with a new substring in each iteration
+        slice = text.slice(start..end) // This is a range
         if (slice == segment) {
-            counter ++ // Counter is updated
+            counter ++ // Counter is incremented
+            // "Start" and "end" are updated to get a new substring next iteration
             start++
             end++
         } else {
-            start++ // Start and end are updated
+            // "Start" and "end" are updated to get a new substring next iteration
+            start++
             end++
         }
     }

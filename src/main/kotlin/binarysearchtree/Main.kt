5f1cfa2dcff95d549745c4632aeb5a062aa6b3df
jhenriquedsilva <josehs555@gmail.com>
2021-09-12 11:52:23 -0300

    Homework finished

--- src/main/kotlin/binarysearchtree/Main.kt
+++ src/main/kotlin/binarysearchtree/Main.kt
@@ -1,6 +1,37 @@
 package binarysearchtree
 
 fun main() {
+    // Elements are inserted
+    val tree = BinarySearchTree<Int>().apply {
+        for (number in listOf(86,11,7,35,170,2,120,21,88,152,31,12,181,28,134,70,1,85,25,77,10,128,198)) {
+            insert(number)
+        }
+    }
+
+    println(tree)
+
+    // A leat node is removed
+    tree.remove(88)
+
+    println(tree)
+
+    // A node with one child is removed
+    tree.remove(181)
+
+    println(tree)
+
+    // A node with two children is removed
+    tree.remove(11)
+
+    println(tree)
+
+    // Verifies if a value is within the tree
+    println("\nDoes the three contain 35? ${tree.contains(35)}\n")
+
+    // Traverses the tree in order
+    tree.root?.traverseInOrder { print("$it ") }
+
+    /*
     val zero = BinaryNode(0)
     val one = BinaryNode(1)
     val five = BinaryNode(5)
@@ -54,4 +85,5 @@ fun main() {
     exampleTree.remove(3)
     println("Tree after removing root:")
     println(exampleTree)
+    */
 }
